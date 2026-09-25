1adf8e999e27697cb87b95f6ff915100bd103ca3
darrennguyen25 <112592859+darrennguyen25@users.noreply.github.com>
2026-09-25 12:14:01 -0700

    Update email

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ import type { Metadata, Site, Socials } from "@types";
 export const SITE: Site = {
   TITLE: "Darren Nguyen",
   DESCRIPTION: "Hello!",
-  EMAIL: "darrennguyen27@proton.me",
+  EMAIL: "dnguyenbusiness616@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
